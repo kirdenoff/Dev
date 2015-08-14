8c9b5b2edc752756632f82fdf86e3e29cdb4a5db
Denis Kiryanov <kirdenoff@yandex.ru>
2015-08-14 17:56:05 +0300

    EF added + context + model for Company

---
+++ Sources/DevRu/DevRu/EF/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using DevRu.EF.DomainModels;
+
+namespace DevRu.EF
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+            : base("DevRuConnection") { }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ Sources/DevRu/DevRu/EF/DomainModels/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace DevRu.EF.DomainModels
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Website { get; set; }
+        public string OfficialName { get; set; }
+        public int EmployeesQty { get; set; }
+        public short EstablishYear { get; set; }
+        public string Description { get; set; }
+    }
+}
